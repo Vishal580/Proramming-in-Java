dbc1c4af4a2e5ceb80269d45b65ae64f1bd0e60f
vishal <vishal@VISHAL>
2022-05-05 15:30:48 +0530

    build and pom file

---
+++ Vishal/src/main/java/Application2.java
@@ -0,0 +1,11 @@
+
+public class Application2 {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		System.out.println("Addition:" + add(2, 3));
+	}
+
+	public static Object add(double d, double e) {
+		return d + e;
+	}
+}
